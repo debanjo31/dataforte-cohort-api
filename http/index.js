c1524800018e71cb37bf93da42168262b96aae8b
Adebanjo Israel <debanjo31@gmail.com>
2025-02-06 12:00:38 +0200

    Implement basic HTTP server with HTML file serving and custom error handling

--- http/index.js
+++ http/index.js
@@ -9,7 +9,10 @@
 //sever reponse code
 
 const http = require("http");
+const https = require("https");
 const url = require("url");
+const fs = require("fs");
+const path = require("path");
 
 // const server = http.createServer((req, res) => {
 //   //return json response
@@ -68,16 +71,70 @@ const url = require("url");
 // });
 
 //get parameters
-const server = http.createServer((req, res) => {
-  console.log(req.url, "url");
-  const parsedUrl = url.parse(req.url, true);
-  console.log(parsedUrl, "parsedUrl");
-  console.log(parsedUrl.query, "query");
-  const query = parsedUrl.query;
-  res.end("The home page");
-});
+// const server = http.createServer((req, res) => {
+//   console.log(req.url, "url");
+//   const parsedUrl = url.parse(req.url, true);
+//   console.log(parsedUrl, "parsedUrl");
+//   console.log(parsedUrl.query, "query");
+//   const query = parsedUrl.query;
+//   res.end("The home page");
+// });
+
+// const server = http.createServer((req, res) => {
+//   res.writeHead(200, {
+//     "Content-Type": "application/json",
+//   });
+//   res.end(JSON.stringify({ message: "Hello World" }));
+// });
+
+//return html
+// const server = http.createServer((req, res) => {
+//   res.writeHead(200, {
+//     "Content-Type": "text/html",
+//   });
+//   res.end("<div><h1>Hello World</h1> <p>This is a paragraph</p></div>");
+// });
+
+// nextjs
 
 //append server to listen to port 5000
-server.listen(5050, () => {
-  console.log("Server is running on port 5050");
+
+const server = http.createServer((req, res) => {
+  const filePath = path.join(__dirname, "index.html");
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      const errFile = path.join(__dirname, "custom-error.html");
+      fs.readFile(errFile, (err, data) => {
+        res.writeHead(404, {
+          "Content-Type": "text/html",
+        });
+        res.end(data);
+      });
+    } else {
+      res.writeHead(200, {
+        "Content-Type": "text/html",
+      });
+      res.end(data);
+    }
+  });
 });
+
+server.listen(5050, () => {
+  console.log("NORMAL HTTP Server is running on port 5050");
+});
+
+// const options = {
+//   key: fs.readFileSync("key.pem"),
+//   cert: fs.readFileSync("cert.pem"),
+// };
+
+// PEM - PRIVACY ENHANCED MAIL FILE- sharing and storing encrypted data
+// SSL/TLS ENCRYPTION
+
+// https.createServer(options, (req, res) => {
+//   res.end("Hello World");
+// });
+
+// httpsServer.listen(5051, () => {
+//   console.log("HTTPS Server is running on port 5051");
+// });
